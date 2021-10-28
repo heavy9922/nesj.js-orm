441d6b199d1c24e5f635a8784e2693b143ada706
--replace-all <--replace-all>
2021-10-27 23:40:11 -0500

    se agrego el entidad alias modelos con el orm junto a su modulo

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,8 +1,17 @@
+import { PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
+
+@Entity()
 export class Product {
+  @PrimaryGeneratedColumn()
   id: number;
+  @Column({type: 'character varying', length: 255, nullable:false, unique:true})
   name: string;
+  @Column({type: 'text'})
   description: string;
+  @Column({type: 'double precision'})
   price: number;
+  @Column({type: 'integer'})
   stock: number;
+  @Column({type: 'text', length: 255})
   image: string;
 }
